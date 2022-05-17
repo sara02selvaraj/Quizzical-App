50ae32d2c62125bbda69ea6e8298c3c999ff5b1b
saranya <selvaraj.umasaranya@gmail.com>
2022-05-16 23:06:14 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 
 function App() {
   const [quiz, setQuiz] = useState([])
+  const [score, setScore] = useState(0)
+  const [displayResult, setDisplayResult] = useState(false)
+
   function handleQuiz() {
     const dataURL = `https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple`
     fetch(dataURL).then(res => res.json())
     .then(data => {
-      console.log(data.results)
-      setQuiz(data.results)
+      setQuiz(data.results.map(item => {
+        return {
+          id: nanoid(),
+          question: item.question,
+          answers: shuffle([...item.incorrect_answers, item.correct_answer]),
+          correctAnswer : item.correct_answer,
+          scored: false
+          
+        }
+      }))
     }
     )
   }
+
+
+  function shuffle(arr) {
+   let array = arr.map(ans => {
+      return {
+        id: nanoid(),
+        answer: ans,
+        isSelected: false
+        
+      }
+    })
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1))
+      let temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    return array
+  }
+
+  function handleSelected(quesId,selectedAnsId) {
+    setQuiz(prevState => prevState.map(item => {
+      if (item.id === quesId) {
+        return {
+          ...item, 
+        answers: item.answers.map(ans => {
+          return ans.id === selectedAnsId ? {...ans, isSelected: !ans.isSelected} : {...ans, isSelected: false}
+        } ),
+        scored: item.correctAnswer == item.answers.find(ans => ans.id === selectedAnsId).answer ? true : false
+        }
+      }
+        else{
+          return item
+        }
+      // return {
+      //   ...item, 
+      //   answers: item.answers.map(ans => {
+      //     return ans.id === ansId ? {...ans, isSelected: !ans.isSelected} : ans
+      //   })
+      // }
+    })
+    )
+  }
+
+  console.log(quiz)
+
+  function handleSubmit() {
+    let count = quiz.filter(item => item.scored)
+    setScore(count.length)
+    setDisplayResult(true)
+  }
+
   return (
     <main>
       <div className="container">
         {
-          quiz.length > 0 ? quiz.map(item => <Quiz question={item.question}/>)
+          quiz.length > 0 ? <div>
+          {
+            quiz.map(item => <Quiz key={item.id} id={item.id} question={item.question}
+              answers= {item.answers} handleSelected={handleSelected} scored={item.scored} displayResult={displayResult}/> 
+            )
+          } 
+          {
+            score > 0 && <p>You scored {score}/5 correct answers</p>
+          }
+          <button onClick={handleSubmit} className='btn-primary'>Check answers</button>
+          </div>
            : 
             <div>
-              <h1>Quizzical</h1>
+              <h1 className='title'>Quizzical</h1>
               <p>Quizzical app to test your knowledge</p>
               <button onClick={handleQuiz} className='btn-primary'>Start Quiz</button>
             </div>
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,32 @@
 export default function Quiz(props) {
+//    function handleBg() {
+//     if(props.displayResult) {
+//         if(item.score) {
+//             backgroundColor: "green"
+//         } else{
+//             if(item.isSelected){
+//                 backgroundColor: "red"
+//             }
+            
+//         }
+//     }else{
+//         if(item.isSelected){
+//             backgroundColor: "#D6DBF5"
+//         }
+//     }
+//    }
+
     return (
-        <div>
-            <h2 className="question">{props.question}</h2>
+        <div className="quiz--container">
+            <h2 className="quiz--question">{props.question}</h2>
+            <div className="quiz--answers">
+               {
+                    props.answers.map(item => <p key={item.id} onClick={() => props.handleSelected(props.id,item.id)} 
+                    className={`answers-item ${!props.displayResult && item.isSelected ? 'bg-class' : ''}`}
+                    style={{backgroundColor: props.displayResult && props.scored && item.isSelected  ?  "#94D7A2" : item.isSelected && !props.scored && props.displayResult ? '#F8BCBC' : ""  }}>{item.answer}</p>)
+                }
+            </div>
+            
         </div>
     )
 }
